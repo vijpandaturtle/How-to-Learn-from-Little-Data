b4621d910d310f8f2975c5c760d3ab426022d0a0
vijpandaturtle <thisisvij98@gmail.com>
2017-05-07 11:30:59 +0530

    Modified generator.py
    Modified generator.py and changed data folder structure

--- MANN/Utils/Generator.py
+++ MANN/Utils/Generator.py
@@ -7,7 +7,7 @@ from Images import get_shuffled_images, time_offset_label, load_transform
 
 class AnimalDataGenerator():
     # This is a re-implementation of the omniglot generator for the animal pictures data.
-    def __init__(self, data_folder, batch_size=1, nb_samples=2, nb_samples_per_class=20, max_rotation=-np.pi/6, max_shift=10, img_size=(20,20), max_iter=None): )
+    def __init__(self, data_folder, batch_size=1, nb_samples=2, nb_samples_per_class=20, max_rotation=-np.pi/6, max_shift=10, img_size=(20,20), max_iter=None):
         self.data_folder = data_folder
         self.batch_size = batch_size
         self.nb_samples = nb_samples
@@ -17,10 +17,9 @@ class AnimalDataGenerator():
         self.img_size = img_size
         self.max_iter = max_iter
         self.num_iter = 0
-        self.character_folders = [os.path.join(self.data_folder, family, character) \
-                                  for family in os.listdir(self.data_folder) \
-                                  if os.path.isdir(os.path.join(self.data_folder, family)) \
-                                  for character in os.listdir(os.path.join(self.data_folder, family))]
+        self.animal_folders = [os.path.join(self.data_folder, data, animal) \
+                                  if os.path.isdir(os.path.join(self.data_folder, data)) \
+                                  for animal in os.listdir(os.path.join(self.data_folder, data))]
 
     def __iter__(self):
         return self
@@ -36,14 +35,14 @@ class AnimalDataGenerator():
             raise StopIteration
 
     def sample(self, nb_samples):
-        sampled_character_folders = random.sample(self.character_folders, nb_samples)
-        random.shuffle(sampled_character_folders)
+        sampled_animal_folders = random.sample(self.character_folders, nb_samples)
+        random.shuffle(sampled_animal_folders)
 
         example_inputs = np.zeros((self.batch_size, nb_samples * self.nb_samples_per_class, np.prod(self.img_size)), dtype=np.float32)
         example_outputs = np.zeros((self.batch_size, nb_samples * self.nb_samples_per_class), dtype=np.float32)     #notice hardcoded np.float32 here and above, change it to something else in tf
 
         for i in range(self.batch_size):
-            labels_and_images = get_shuffled_images(sampled_character_folders, range(nb_samples), nb_samples=self.nb_samples_per_class)
+            labels_and_images = get_shuffled_images(sampled_animal_folders, range(nb_samples), nb_samples=self.nb_samples_per_class)
             sequence_length = len(labels_and_images)
             labels, image_files = zip(*labels_and_images)
 
